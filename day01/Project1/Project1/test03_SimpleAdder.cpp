294f89889dbda9682f5d94fd1295b126c133401c
Geuna00 <rmsdk617@naver.com>
2024-02-21 13:23:18 +0900

    1일차 오전

---
+++ day01/Project1/Project1/test03_SimpleAdder.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+/*
+int global = 100; // 전역변수. 프로그램 어디에서든 사용가능
+
+void func() {
+	int a;
+	a = 100; // 지역변수. func() 함수 안에서만 사용가능
+	global = 200;
+}
+
+int main() {
+
+	int a;
+	int val; // (지역변수) 메모리공간(RAM)에 int크기로 저장공간을 할당받고 val이름으로 사용한다.
+	std::cout << "숫자입력 : ";
+	std::cin >> val; // 입력을 받아서 >> 오른쪽 변수에 저장
+
+	std::cout << "입력된 숫자는 " << val <<"입니다." << std::endl;
+	a = 10;
+	val = 100;
+	global = 300;
+	return 0;
+}
+
+/*
+* 지역변수 = 로컬변수 : 선언된 범위안에서만 사용이 가능(static 영역에 저장)
+*/
---
+++ day01/Project1/Project1/test05_for01.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std; // main()함수 안의 std 선언을 생략할 수 있다.
+
+int main() {
+
+	// 변수선언을 할때 코드의 가독성을 높이기 위해 한곳에 모아서 작성하기
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int ary[3] = { 10 ,20, 30 };
+	ary[0] = 1111; // 인덱스[0]에 1111을 집어넣어라
+
+	for (int i = 0; i < 10; i++) {
+		cout << i << endl;
+	}
+	return 0;
+}
