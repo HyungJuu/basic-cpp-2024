a6808afed3d00081251b56dc4555a323cef6b6e5
Geuna00 <rmsdk617@naver.com>
2024-02-27 17:26:58 +0900

    변수, 함수, 제어문 복습하기

--- day05/Project3/Project3/test13_copyCon02.cpp
+++ day05/Project3/Project3/test13_copyCon02.cpp
@@ -8,7 +8,7 @@ private:
 	int b;
 
 public:
-	AA(int a, int b)
+	AA(int a, int b)		// 생성자
 	{
 		this->a = a;
 		this->b = b;
@@ -22,7 +22,11 @@ public:
 
 int main()
 {
-	AA obj1(10, 20);
+	AA obj1(10, 20);	// 입력값 두개를 받는 생성자를 호출
 	obj1.AAInfo();
+	
+	AA obj2(obj1);		// 복사생성자를 만들지 않았지만 호출됨 -> 컴파일러가 자동으로 삽입 (디폴트 복사생성자)
+	obj2.AAInfo();
+
 	return 0;
 }
---
+++ day05/Project3/Project3/test14_copyCon03.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class AA
+{
+private:
+	int a;
+	int b;		
+public:
+	AA(int a, int b)	// 생성자(초기화)
+	{
+		this->a = a;
+		this->b = b;
+	}
+
+	AA(const AA& r)		// 복사생성자
+	{
+		this->a = r.a;
+		this->b = r.b;
+	}
+
+	void AAInfo()
+	{
+		cout << a << " " << b << endl;
+	}
+};
+
+int main()
+{
+	AA obj1(10, 20);	// 객체 obj1 생성 및 초기화
+	obj1.AAInfo();
+
+	AA obj2(obj1);		// 
+	obj2.AAInfo();
+
+	return 0;
+}
