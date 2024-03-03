d4c170cf0f5b5589c1f4d2f1be4a37e398460053
HyungJuu <rmsdk617@naver.com>
2024-03-03 17:36:06 +0900

    01-1 복습 및 문제풀이

---
+++ 복습용/Project1/Project1/01_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> // 헤더파일 선언문
+
+int main()
+{
+	int val1, val2;		// int 타입(정수) 두개 선언 : 지역변수 -> 함수 내에서만 사용가능하다.
+	std::cout << "숫자 두개를 입력하세요 > ";		// std::cout << 출력대상;
+	std::cin >> val1 >> val2;	// 이처럼 변수값을 연달아 입력받을 수 있다. ex) 4 5
+	//std::cin >> val1;
+	//std::cin >> val2;
+
+	std::cout << "입력된 숫자는 " << val1 << ", " << val2 << "입니다." << std::endl;	// std::endl; 은 엔터와 같은 기능을 한다. 개행으로 출력된다.
+	
+	char name[20];
+	std::cout << "이름을 입력하세요 : ";
+	std::cin >> name;
+	
+	std::cout << "이름 : " << name << std::endl;
+	return 0;
+}
---
+++ 복습용/Project1/Project1/01_1_문제2.cpp
@@ -0,0 +1,22 @@
+﻿/* 문제2
+* 프로그램 사용자로부터 이름과 전화번호를 문자열의 형태로 입력받아서,
+* 입력받은 데이터를 그대로 출력하는 프로그램을 작성해보자.
+*/
+
+#include <iostream>
+
+int main()
+{
+	char name[20];
+	char pnum[20];
+	
+	std::cout << "이름을 입력하세요 >> ";
+	std::cin >> name;
+	std::cout << "전화번호를 입력하세요 >> ";
+	std::cin >> pnum;
+
+	std::cout << "이름 : " << name << std::endl;
+	std::cout << "전화번호 : " << pnum << std::endl;
+
+	return 0;
+}
