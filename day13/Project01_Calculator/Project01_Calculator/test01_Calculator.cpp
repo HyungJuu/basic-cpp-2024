7bc4904250e08bebecd40d880cf6c385d541cd0c
Geuna00 <rmsdk617@naver.com>
2024-03-11 09:49:03 +0900

    upload

---
+++ day13/Project01_Calculator/Project01_Calculator/test01_Calculator.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int n1, n2;
+	char cal;
+	cout << "정수 2개를 입력하세요 >> ";
+	cin >> n1 >> n2;
+
+	cout << "계산하고자 하는 연산자를 입력하세요 >> ";
+	cin >> cal;
+
+	if (cal == '+')
+		cout << n1 << '+' << n2 << '=' << n1 + n2 << endl;
+
+	else if (cal == '-')
+		cout << n1 << '-' << n2 << '=' << n1 - n2 << endl;
+
+	else if (cal == '*')
+		cout << n1 << '*' << n2 << '=' << n1 * n2 << endl;
+	
+	else if (cal == '/')
+
+	try		// 예외처리
+	{
+		if (!n2) throw n2;	// n2가 0이 아니면(true)이면 n2값으로 그대로 계산
+		cout << n1 << '/' << n2 << '=' << n1 / n2 << endl;
+	}
+	catch (int exception)
+	{
+		cout << "0 으로 나눌수 없습니다." << endl;
+	} 
+
+	return 0;
+}
---
+++ day13/Project01_Calculator/Project01_Calculator/test02_Calculator02.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void Calculator(int _n1, int _n2)
+{
+	cout << "+ : " << _n1 + _n2 << endl;
+	cout << "- : " << _n1 - _n2 << endl;
+	cout << "* : " << _n1 * _n2 << endl;
+
+	if (!_n2) throw _n2;
+	cout << "/ : " << _n1 / _n2 << endl;
+}
+
+int main(int argc, char* argv[])
+{
+	int n1, n2;
+	cout << "정수 2개를 입력하세요 >> ";
+	cin >> n1 >> n2;
+	try
+	{
+		Calculator(n1, n2);
+	}
+	catch (int exception)
+	{
+		cout << "0으로 나눌 수 없습니다." << endl;
+	}
+
+	return 0;
+}
